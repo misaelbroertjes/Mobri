238917b25c0fbf3202042ae02abf7c279657fc28
misaelbroertjes <misaelbroertjes@hotmail.com>
2017-12-05 13:32:20 +0100

    update to tmp v2

---
+++ home.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Mobri | Home</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional Bootstrap theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- None bootstrap -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+	<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<script src="js/scripts.js"></script>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<!-- Header -->
+<div id="headerWrapper" class="bgGroen borderGeelBtm">
+	<div class="container">
+		<div id="header" class="row">
+			<div class="col-sm-4 col-sm-offset-1">
+				<img src="images/logo_mobri.jpg" alt="logo Mobri" class="borderGeelBtm" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Intro -->
+<div class="container">
+	<div id="intro" class="row">
+		<div id="vlak" class="col-sm-10 col-sm-offset-1 bgGeel">
+			<h2>Misaël Broertjes</h2>
+			<p>
+				Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id.
+			</p>
+			<p>
+				Quisque pellentesque quam vitae varius iaculis. Quisque at orci ac mi sollicitudin gravida. Vestibulum mattis urna vel scelerisque fermentum. Ut in porttitor nibh.
+			</p>
+			<p>
+				Pellentesque habitant morbi tristique senectus et netuset.
+				tibulum sapien augue, convallis nec ultrices ac, porttitor vel.
+			</p>
+		</div>
+		<div id="foto" class="col-sm-12 col-sm-offset-0">
+      <img src="images/foto_tmp.png" alt="Misaël Broertjes" class="" />
+		</div>
+	</div>
+</div>
+
+<!-- Technieken -->
+<div class="container">
+	<div id="technieken" class="row bgGroen">
+		<div class="col-sm-12 col-sm-offset-0 text-center">
+			<span>Wij bouwen met de nieuwste technieken</span>
+		</div>
+	</div>
+
+	<div id="techniekenOverzicht" class="row bgGeel">
+		<div id="techniekenBootstrap" class="col-sm-2 col-sm-offset-2 ">
+			<img src="images/tech_bs.png" alt="Twitter Bootstrap" class="img-responsive" />
+		</div>
+		<div id="techniekenHtml5" class="col-sm-2 col-sm-offset-0">
+			<img src="images/tech_html.png" alt="html5" class="img-responsive" />
+		</div>
+		<div id="techniekenCss3" class="col-sm-2 col-sm-offset-0">
+			<img src="images/tech_css.png" alt="css3" class="img-responsive" />
+		</div>
+		<div id="techniekenJquery" class="col-sm-2 col-sm-offset-0">
+			<img src="images/tech_jq.png" alt="jQuery" class="img-responsive" />
+		</div>
+	</div>
+</div>
+
+<!-- Skills -->
+<div id="skillsWrapper" class="bgGroen">
+	<div class="container">
+		<div id="skills" class="row">
+			<div class="col-sm-12 col-sm-offset-0">
+					<h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;skills</h1>
+			</div>
+
+			<div class="col-sm-4 col-sm-offset-0">
+				<h2>Coding</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id. Donec convallis mi vel scelerisque laoreet. Ut turpis erat, porta et elementum et, tempor sit amet est. Ut ultrices orci ultrices sapien viverra vulputate.</p>
+				<p class="text-center">70%</p>
+				<div class="progress">
+				  <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar"
+				  aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+				  </div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-sm-offset-0">
+				<h2>Styling</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id. Donec convallis mi vel scelerisque laoreet. Ut turpis erat, porta et elementum et, tempor sit amet est. Ut ultrices orci ultrices sapien viverra vulputate.</p>
+				<p class="text-center">90%</p>
+				<div class="progress">
+				  <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar"
+				  aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width:90%">
+				  </div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-sm-offset-0">
+				<h2>Design</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id. Donec convallis mi vel scelerisque laoreet. Ut turpis erat, porta et elementum et, tempor sit amet est. Ut ultrices orci ultrices sapien viverra vulputate.</p>
+				<p class="text-center">50%</p>
+				<div class="progress">
+				  <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar"
+				  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Mobri -->
+<div id="mobriWrapper" class="container">
+	<div id="mobri" class="row">
+		<div class="col-sm-12 col-sm-offset-0">
+				<h1 class="alt colorGroen">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mobri</h1>
+		</div>
+		<div class="col-sm-9 col-sm-offset-0">
+			<h2 class="colorGroen">
+				Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+			</h2>
+			<p>
+				Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id. Donec convallis mi vel scelerisque laoreet. Ut turpis erat, porta et elementum et, tempor sit amet est. Ut ultrices orci ultrices sapien viverra vulputate.
+			</p>
+			<h3 class="colorGroen">
+				Integer pharetra quam quis porta sollicitudin
+			</h3>
+			<p>
+				Quisque pellentesque quam vitae varius iaculis. Quisque at orci ac mi sollicitudin gravida. Vestibulum mattis urna vel scelerisque fermentum. Vivamus vestibulum nisl sapien, semper gravida dui dictum quis. Donec eget tortor non massa commodo iaculis nec ut mauris. Aenean pretium urna turpis, sollicitudin ultrices elit rhoncus sit amet. Fusce porta nibh non tincidunt porta. In malesuada vitae orci vitae condimentum. Mauris in felis a diam volutpat viverra non at libero. Ut in porttitor nibh.
+			</p>
+			<p>
+				Pellentesque habitant morbi tristique senectus et netuset.
+				tibulum sapien augue, convallis nec ultrices ac, porttitor vel.
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- Werk -->
+<div id="werkWrapper" class="bgGroen">
+	<div  class="container">
+		<div id="werk" class="row">
+			<div class="col-sm-12 col-sm-offset-0">
+					<h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Werk</h1>
+			</div>
+
+
+		  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+		    <!-- Indicators -->
+		    <ol class="carousel-indicators">
+		      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		      <li data-target="#myCarousel" data-slide-to="1"></li>
+		      <li data-target="#myCarousel" data-slide-to="2"></li>
+		    </ol>
+
+		    <!-- Wrapper for slides -->
+		    <div class="carousel-inner">
+
+					<!-- Item 01 -->
+		      <div class="item active">
+						<div class="col-sm-4  col-sm-offset-1">
+							<h2>
+								dehoogbouwassist.nl
+							</h2>
+							<p>
+								Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id. Donec convallis mi vel scelerisque laoreet. Ut turpis erat, porta et elementum et, tempor sit amet est. Ut ultrices orci ultrices sapien viverra vulputate.
+							</p>
+							<p>
+								Quisque pellentesque quam vitae varius iaculis. Quisque at orci ac mi sollicitudin gravida. Vestibulum mattis urna vel scelerisque fermentum. Vivamus vestibulum nisl sapien, semper gravida dui dictum quis.
+							</p>
+							<ul>
+								<li>
+									Pellentesque habitant morbi tristique senectus
+								</li>
+								<li>
+									et netuset tibulum sapien augue, convallis nec ultrices ac
+							 	</li>
+								<li>
+									porttitor vel.
+								</li>
+							</ul>
+						</div>
+						<div class="col-sm-6 col-sm-offset- 0">
+							<img src="images/werk/dehoogbouwassist_nl.png" alt="dehoogbouwassist.nl" class="img-responsive pull-right" />
+						</div>
+		      </div>
+
+					<!-- Item 02 -->
+		      <div class="item">
+						<div class="col-sm-4  col-sm-offset-1">
+							<h2>
+								dehoogbouwassist.nl
+							</h2>
+							<p>
+								Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id. Donec convallis mi vel scelerisque laoreet. Ut turpis erat, porta et elementum et, tempor sit amet est. Ut ultrices orci ultrices sapien viverra vulputate.
+							</p>
+							<p>
+								Quisque pellentesque quam vitae varius iaculis. Quisque at orci ac mi sollicitudin gravida. Vestibulum mattis urna vel scelerisque fermentum. Vivamus vestibulum nisl sapien, semper gravida dui dictum quis.
+							</p>
+							<ul>
+								<li>
+									Pellentesque habitant morbi tristique senectus
+								</li>
+								<li>
+									et netuset tibulum sapien augue, convallis nec ultrices ac
+							 	</li>
+								<li>
+									porttitor vel.
+								</li>
+							</ul>
+						</div>
+						<div class="col-sm-6 col-sm-offset- 0">
+							<img src="images/werk/dehoogbouwassist_nl.png" alt="dehoogbouwassist.nl" class="img-responsive pull-right" />
+						</div>
+		      </div>
+
+					<!-- Item 03 -->
+		      <div class="item">
+						<div class="col-sm-4  col-sm-offset-1">
+							<h2>
+								dehoogbouwassist.nl
+							</h2>
+							<p>
+								Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus felis nibh, et feugiat eros hendrerit id. Donec convallis mi vel scelerisque laoreet. Ut turpis erat, porta et elementum et, tempor sit amet est. Ut ultrices orci ultrices sapien viverra vulputate.
+							</p>
+							<p>
+								Quisque pellentesque quam vitae varius iaculis. Quisque at orci ac mi sollicitudin gravida. Vestibulum mattis urna vel scelerisque fermentum. Vivamus vestibulum nisl sapien, semper gravida dui dictum quis.
+							</p>
+							<ul>
+								<li>
+									Pellentesque habitant morbi tristique senectus
+								</li>
+								<li>
+									et netuset tibulum sapien augue, convallis nec ultrices ac
+							 	</li>
+								<li>
+									porttitor vel.
+								</li>
+							</ul>
+						</div>
+						<div class="col-sm-6 col-sm-offset- 0">
+							<img src="images/werk/dehoogbouwassist_nl.png" alt="dehoogbouwassist.nl" class="img-responsive pull-right" />
+						</div>
+		      </div>
+		    </div>
+
+		    <!-- Left and right controls -->
+		    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		      <span class="glyphicon glyphicon-chevron-left"></span>
+		      <span class="sr-only">Previous</span>
+		    </a>
+		    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+		      <span class="glyphicon glyphicon-chevron-right"></span>
+		      <span class="sr-only">Next</span>
+		    </a>
+		  </div>
+
+		</div>
+	</div>
+</div>
+
+<!-- Maps -->
+<div class="container-fluid">
+	<div id="maps" class="row">
+		<div class="col-sm-12 col-sm-offset-0">
+			maps
+		</div>
+	</div>
+</div>
+
+<!-- Contact -->
+<div class="container">
+	<div id="contact" class="row">
+		<div class="col-sm-5 col-sm-offset-1" id="contactInfo">
+			<table>
+				<tr>
+					<td width="30"></td>
+					<td><h2 class="colorGroen">Mobri</h2></td>
+				</tr>
+				<tr>
+					<td><i class="fa fa-user fa-lg faGeel" aria-hidden="true"></i></td>
+					<td><p>Misaël Broertjes</p></td>
+				</tr>
+				<tr>
+					<td><i class="fa fa-mobile fa-2x" aria-hidden="true"></i></td>
+					<td><p>06 38125176</p></td>
+				</tr>
+				<tr>
+					<td><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i></td>
+					<td><p>Weidemaat 18, Meppel, Nederland</p></td>
+				</tr>
+			</table>
+		</div>
+
+		<div class="col-sm-4 col-sm-offset-1">
+
+			<form method="post" name="contactForm" id="contactForm" >
+				<h2 class="text-uppercase text-center colorGroen">Contact</h2>
+			  <div class="form-group">
+			    <input type="text" class="form-control" id="naam" aria-describedby="" placeholder="Naam">
+			  </div>
+			  <div class="form-group">
+			    <input type="text" class="form-control" id="emailtel" aria-describedby="" placeholder="E-mail of telefoonnummer">
+			  </div>
+				<div class="form-group">
+			    <input type="text" class="form-control" id="onderwerp" aria-describedby="" placeholder="Onderwerp">
+			  </div>
+				<div class="form-group">
+			    <textarea class="form-control" id="bericht" rows="3" placeholder="Bericht"></textarea>
+			  </div>
+			  <button type="submit" class="btn col-sm-12 verzenden">verzenden</button>
+			</form>
+
+		</div>
+	</div>
+</div>
+
+<!-- Footer -->
+<div class="container-fluid" id="footerWrapper">
+	<div id="footer-top" class="row bgGroen">
+		<div class="col-sm-12 col-sm-offset-0">
+		</div>
+	</div>
+	<div id="footer" class="row bgGeel">
+		<div class="col-sm-12 col-sm-offset-0 text-center">
+				<span class="colorGroen">© 2017 Alle rechten voorbehouden - Mobri - K.v.K. 66470188 - BTW NL194697678B01</span>
+		</div>
+	</div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
